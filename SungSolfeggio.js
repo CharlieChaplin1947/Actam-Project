8a82f863c551819c8930099b41f2e70c16691c42
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2020-01-09 15:16:27 +0100

    Fixed SungSolfeggioHelper

--- SungSolfeggio.js
+++ SungSolfeggio.js
@@ -12,7 +12,9 @@ var app=new Vue({
         scoresheet_heigth:150,
         frequencies:[],
         generatedNotes:[],
-        noteLabels:[]
+        noteLabels:[],
+        flag:false,
+        indications:String
     },
     methods:{
         initialiseFrequencies: function(){
@@ -38,34 +40,28 @@ y=100;
 noteNumber=0;
 vx=1;
 vy=0;
-var dataArray=new Float32Array();
-
-function draw(){
-    requestAnimationFrame(draw);
-    ctx.beginPath();
-    ctx.clearRect(0,0,app.scoresheet_width,app.scoresheet_heigth);
-    ctx.arc(x,app.scoresheet_heigth/7,app.scoresheet_heigth/7/2,0,2*Math.PI);
-    var i;
-    for (i=1;i<6;i++){
-        ctx.moveTo(0,app.scoresheet_heigth/7*i);
-        ctx.lineTo(app.scoresheet_width,app.scoresheet_heigth/7*i);
-        ctx.stroke();
-    }
-    x-=vx;
-}
 
 function drawSolfeggio(){
     requestAnimationFrame(drawSolfeggio);
+    if(app.flag==true){
     ctx.beginPath();
     ctx.clearRect(0,0,app.scoresheet_width,app.scoresheet_heigth);
     var noteSize=app.scoresheet_heigth/7/2;
     if(noteNumber<8){
         var index=app.frequencies.indexOf(app.generatedNotes[noteNumber]);
-        ctx.arc(x,app.scoresheet_heigth-index*noteSize-noteSize*2,noteSize,0,2*Math.PI);
+        var noteHeigth=app.scoresheet_heigth-index*noteSize-noteSize*2;
+        ctx.arc(x,noteHeigth,noteSize,0,2*Math.PI);
         ctx.stroke();
         x-=vx;
     };
+    if(index==0){
+        ctx.moveTo(x-20,noteHeigth);
+        ctx.lineTo(x+20,noteHeigth);
+        ctx.stroke();
+    }
     if(x==0){
+        if(noteNumber==7){app.flag=false;
+        app.indications="Solfeggio finished"}
         noteNumber++;
         x=app.scoresheet_width;
     };
@@ -73,7 +69,7 @@ function drawSolfeggio(){
     for (i=1;i<6;i++){
         ctx.moveTo(0,app.scoresheet_heigth/7*i);
         ctx.lineTo(app.scoresheet_width,app.scoresheet_heigth/7*i);
-        ctx.stroke();
+        ctx.stroke();}
     };
 }
 
@@ -85,44 +81,58 @@ drawSolfeggio();
 
 var audio_ctx=new AudioContext();
 var source;
+var analyser=audio_ctx.createAnalyser();
 //the following catches stream from the microphone
 const handleSuccess=function(stream){
     if(window.URL){
-        start_time=audio_ctx.currentTime;
-        var j;
-        j=start_time;
-        console.log(stream);
-        source=audio_ctx.createMediaStreamSource(stream);
-        console.log(source);
-        var analyser=audio_ctx.createAnalyser();
-        analyser.minDecibels=-90;
-        analyser.maxDecibels=-10;
-        source.connect(analyser);
-        result=new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteTimeDomainData(result);  //Sees a silent stream
-        while(j<start_time+1000){
-        console.log(result);
-        console.log(analyser);
-        j+=1}
-        source.connect(audio_ctx.destination);
-        
-        
-
-        //var mediaRecorder=new MediaRecorder(stream);
-        //mediaRecorder.start(100);
-        //mediaRecorder.ondataavailable=function(e){
-        //    var temp;
-        //    temp=e.data;
-        //    console.log(temp);
-        //    console.log(temp.stream());
-        }
-    else{
-        //do something else
-        source=audio_ctx.createMediaStreamSource(stream);
-        console.log(dataArray);
+         var source=audio_ctx.createMediaStreamSource(stream);
+         source.connect(analyser);
+         app.flag=true;       
     }
+    else{}
 }
 
-
-
 navigator.mediaDevices.getUserMedia({audio:true, video:false}).then(handleSuccess);
+
+var bufferLength=analyser.frequencyBinCount;
+var dataArray=new Uint8Array(bufferLength);
+analyser.getByteFrequencyData(dataArray);
+
+function clipFrequencies(array){
+    var temp=new Uint8Array(array.length);
+    for(var i=0;i<array.length;i++){
+        var current_frequency=i/array.length*audio_ctx.sampleRate/2;
+        if(current_frequency>261){
+            if(current_frequency<494){
+            temp[i]=array[i];}
+        }
+    };
+    return temp;
+}
+
+function checkFrequency(){
+    analyser.getByteFrequencyData(dataArray);
+    dataArray=clipFrequencies(dataArray);
+    var tolleranceRange=20;
+    //the frequency range in the array is linear from 0 to half the sample rate of audio_ctx
+    var sungFrequency=dataArray.indexOf(Math.max(...dataArray))/dataArray.length*audio_ctx.sampleRate/2;
+    console.log("la frequenza cantata è:");
+    console.log(sungFrequency);
+    lowerLimit=app.generatedNotes[noteNumber]-tolleranceRange;
+    upperLimit=app.generatedNotes[noteNumber]+tolleranceRange;
+    if(sungFrequency<lowerLimit){
+        if(app.flag==false){app.indications="";}
+        else{
+        app.indications="you have to go Higher!"}
+    }else{
+    if(sungFrequency>upperLimit){
+        app.indications="you have to go lower!"
+    }else{
+        if(app.flag==false){app.indications="Solfeggio finished";}else{
+        app.indications="that's exactely right!!";}
+    }
+        }
+    
+}
+
+setInterval(checkFrequency,100);
